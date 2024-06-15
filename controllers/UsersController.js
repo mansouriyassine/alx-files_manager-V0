42bb235553180c4a4f96a45653c1b2f78b03a3a6
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-15 16:58:37 +0100

    3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class UserController {
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: dbClient.ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
+}
+
+module.exports = UserController;
