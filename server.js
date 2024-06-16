72585a29653c1b8c71a8fcdac8018e95893c004b
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-16 16:49:44 +0100

    2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes/index.js';
 
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
